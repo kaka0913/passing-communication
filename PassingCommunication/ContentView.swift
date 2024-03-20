df7937174196f9090e9eb535ba2d63eb8f258e05
kaka0913 <kabuyuu0913@gmail.com>
2024-03-21 01:57:10 +0900

    Initial Commit

---
+++ PassingCommunication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PassingCommunication
+//
+//  Created by 株丹優一郎 on 2024/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PassingCommunication/PassingCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingCommunicationApp.swift
+//  PassingCommunication
+//
+//  Created by 株丹優一郎 on 2024/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PassingCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
